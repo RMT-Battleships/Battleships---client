f72880f854677c93427b99ce22a2b7f132c5793f
Miloš Janković <mjankovic180@gmail.com>
2020-08-24 21:47:08 +0200

    waiting screen
    waiting screen logic needs to be added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,12 +11,13 @@ import { GameComponent } from './game/game.component';
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
