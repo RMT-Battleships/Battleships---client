37d4a19ec917c4813173f968be8b69768bef03c0
Miloš Janković <mjankovic180@gmail.com>
2020-08-25 23:29:54 +0200

    Updated strategy component
    Added ship interface, initialized instance of ship service and used method for creating ships.
    Also catched event from board component.

--- src/app/game/strategy/strategy.component.ts
+++ src/app/game/strategy/strategy.component.ts
@@ -2,19 +2,37 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { Board } from "../interfaces/board";
 import { BoardService } from "../services/board.service";
+import { ShipService } from "../services/ship.service";
+import { Ship } from "../interfaces/ship";
 
 @Component({
   selector: 'app-strategy',
   templateUrl: './strategy.component.html',
-  styleUrls: ['./strategy.component.css']
+  styleUrls: ['./strategy.component.css'],
+  providers: [ShipService]
 })
 export class StrategyComponent implements OnInit {
   board: Board;
+  selectedShip: Ship;
+  coordinates: { x: number, y: number };
 
-  constructor(private boardService: BoardService) { }
+  constructor(private boardService: BoardService,
+              private shipService: ShipService) { }
 
   ngOnInit(): void {
     this.board = this.boardService.getBoards()[0];
+    this.shipService.createShips();
+  }
+
+  setSelectedShip(element) {
+    this.selectedShip = element;
+  }
+
+  setCoordinates(coordinates) {
+    this.coordinates = {
+      x: coordinates.row,
+      y: coordinates.col
+    }
   }
 
 }
