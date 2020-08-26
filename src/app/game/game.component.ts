c1320433b78efa4134a82d1c836c6bfbf1936111
Miloš Janković <mjankovic180@gmail.com>
2020-08-26 03:59:31 +0200

    Updated game component
    Initialized instance of board service for component and subcomponents, added requirements and called function from board service to create boards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BoardService } from "./services/board.service";
+
+const REQUIRED_NUMBER_OF_PLAYERS: number = 2;
+const BOARD_SIZE: number = 10;
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
+  providers: [BoardService]
 })
 export class GameComponent implements OnInit {
+  gameId: string;
 
-  constructor() { }
+  constructor(private boardService: BoardService) { }
 
   ngOnInit(): void {
+    this.createBoards();
   }
 
+  createBoards() : GameComponent {
+    for (let i = 0; i < REQUIRED_NUMBER_OF_PLAYERS; i++)
+      this.boardService.createBoard(BOARD_SIZE);
+    return this;
+  }
 }
