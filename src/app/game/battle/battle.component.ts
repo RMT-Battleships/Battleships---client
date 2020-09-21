129d835ea3b1f7012fdb74d6b22d62b6ba7254cc
Miloš Janković <mjankovic180@gmail.com>
2020-09-21 14:30:50 +0200

    updating grids

--- src/app/game/battle/battle.component.ts
+++ src/app/game/battle/battle.component.ts
@@ -13,11 +13,23 @@ export class BattleComponent implements OnInit {
   @Input() reuiredNumberOfPlayersForGame: number = 2;
 
   player: number = 0;
+  isOnTurn = 0;
   currentNumberOfPlayersInRoom: number = 2; //for now it is hardcoded, later it will be sent from server
 
   constructor(private boardService: BoardService,private webSocketService: WebSocketService ) { }
 
   ngOnInit(): void {
+    this.webSocketService.listen('update').subscribe((data: any) => {
+        this.isOnTurn = data.turn;
+        let matrix = [];
+        for (let i = 0; i < 10; i++){
+          matrix.push([]);
+          for(let j = 0; j < 10; j++){
+            matrix[i].push(data.grid[i * 10 + j]);
+          }
+        }
+        this.boardService.boards[data.gridIndex].cells = data.matrix;
+    });
   }
 
   get boards () : Board[] {
@@ -25,6 +37,6 @@ export class BattleComponent implements OnInit {
   }
 
   fireTorpedo(data){
-    this.webSocketService.emit('torpedo',[data.col,data.row]);
+    this.webSocketService.emit('torpedo',[data.col, data.row]);
   }
 }
